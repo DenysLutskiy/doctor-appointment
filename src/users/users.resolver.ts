5559f03c3edc9fd88bc3e186a90c1a97c687a4a3
Denis Lutskyi <d.lutskyi@futuro.team>
2021-07-14 14:22:23 +0300

    FT-46 Ability to Identify User

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,7 +1,10 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { CreateUserInput } from './dto/create-user.input';
 import { SigninUserInput } from './dto/signin-user.input';
+import { UseGuards } from '@nestjs/common';
+import { UserGuard } from 'src/guards/user.guard';
+import { User } from './entities/user.entity';
 
 @Resolver('User')
 export class UsersResolver {
@@ -15,6 +18,12 @@ export class UsersResolver {
   @Mutation('signinUser')
   signin(@Args('signinUserInput') signinUserInput: SigninUserInput) {
     return this.usersService.signinWithLoginAndPassword(signinUserInput);
+  }
+
+  @Query('getMe')
+  @UseGuards(UserGuard)
+  me(@Context('user') user: User) {
+    return user;
   }
 
   @Query('users')
