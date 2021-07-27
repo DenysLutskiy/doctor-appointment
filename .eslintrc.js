5c4e199f669e4d9fe81b71d5108d78375c62bb76
Denis Lutskyi <d.lutskyi@futuro.team>
2021-07-28 00:05:25 +0300

    FT-65/fix/ code refactoding and regenerated CreateAppointmentTable migration

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,11 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-function-return-type': 1,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
